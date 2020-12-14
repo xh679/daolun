a9ba7a986208072be10f46b24633b75609fc6922
xh679 <75968473+xh679@users.noreply.github.com>
2020-12-14 15:15:39 +0800

    Add files via upload

---
+++ EDFT/utils/util.js
@@ -0,0 +1,12 @@
+/**
+ * 工具类 util.js
+ */
+function formatTime(time, format) {
+  let temp = '0000000000' + time
+  let len = format.length
+  return temp.substr(-len)
+}
+
+module.exports = {
+  formatTime: formatTime
+}
